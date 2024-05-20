8b30300f7b0360dc5be7cad3feac320ea27b3daa
bishn <bishnu.khulal@student.griffith.ie>
2024-05-20 02:04:47 +0100

    changes made

--- src/models/answers.ts
+++ src/models/answers.ts
@@ -1,29 +1,16 @@
-import mongoose, { Schema, Document } from 'mongoose';
-
-export interface Answer extends Document {
+export interface Answer {
+    id: number;
     questionId: number;
     text: string;
     userId: number;
     upvote: number;
-}
-
-const answerSchema = new Schema<Answer>({
-    questionId: {
-        type: Number,
-        required: true,
-    },
-    text: {
-        type: String,
-        required: true,
-    },
-    userId: {
-        type: Number,
-        required: true,
-    },
-    upvote: {
-        type: Number,
-        default: 0,
-    },
-});
-
-export default mongoose.model<Answer>('Answer', answerSchema);
+    downvote: number;
+    comments: Comment[];
+  }
+  
+  export interface Comment {
+    id: number;
+    text: string;
+    userId: number;
+  }
+  
